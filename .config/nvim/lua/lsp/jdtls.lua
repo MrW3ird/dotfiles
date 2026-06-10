367d74e36f07f26061bce4ff13b070efc3973814
Umair <umairrafiq156@gmail.com>
2026-06-10 10:50:32 +0530

    default to java-21

--- .config/nvim/lua/lsp/jdtls.lua
+++ .config/nvim/lua/lsp/jdtls.lua
@@ -4,7 +4,16 @@ vim.api.nvim_create_autocmd('FileType', {
         local config = {
             cmd = { "jdtls" },
             root_dir = vim.fs.root(0, { 'gradlew', '.git', 'mvnw' }),
-            capabilities = require('cmp_nvim_lsp').default_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+            settings = {
+                java = {
+                    configuration = {
+                        runtimes = {
+                            { name = "JavaSE-21", path = "/usr/lib/jvm/java-21-openjdk" },
+                        }
+                    }
+                }
+            }
         }
         require('jdtls').start_or_attach(config)
     end
